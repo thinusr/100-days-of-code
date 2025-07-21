53369550fa25c5d5f052e223d16420fc5c6d9daa
Thinus <thinusr@gmail.com>
2025-07-21 10:29:07 +0200

    Deleted Day 1

--- Day 1/bandname-generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome To The Band Name Generator")
-city = input("Which city did you grow up in? \n")
-pet_name = input("What is your pet's name? \n")
-print("Your band name could be: " + city + " " + pet_name)
