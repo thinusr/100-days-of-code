6e40cdb27fa6f984647a6689fb290b11612c0057
Thinus <thinusr@gmail.com>
2025-07-25 10:36:00 +0200

    modified rollercoaster.py

--- Day-3/rollercoaster.py
+++ Day-3/rollercoaster.py
@@ -1,13 +1,29 @@
-print("Welcome to the rollercoaster!")
-height = int(input("Please enter your height in meters: "))
+child_ticket = 5 # under 12
+youth_ticket = 7 # 12-18
+adult_ticket = 12 # 18 and over
+price = 0
+photo = 3
+
+print("Welcome to the rollercoaster ride!")
+height = int(input("Please enter your height in cm. "))
 if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("Please enter you age: "))
-    if age <= 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")
+    print("You can ride the rollercoaster")
+    age = int(input("Please enter your age: "))
+    if age < 12:
+        price = child_ticket
+        print(f"Your ticket price will be ${price}")
+    elif age < 18:
+        price = youth_ticket
+        print(f"Your ticket price will be ${price}")
     else:
-        print("Please pay $12.")
+        price = adult_ticket
+        print (f"Your ticket price will be ${price}")
+
+    wants_photo = input(f"Would you like your photo taken at an additional ${photo}? Enter Y for Yes and N for No. ")
+    if wants_photo.upper() == "Y":
+        print(f"Thank you for choosing this option.  Your final bill will be ${price + photo}")
+    else:
+        print(f"Thank you for your patronage.  Your final bill will be ${price}")
+
 else:
-    print("Sorry, you have to grow taller before you can ride the rollercoaster.")
+    print("Sorry, you are too short for this ride and need to grow taller.")
